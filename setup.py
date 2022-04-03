3492de635661bfaa2bdf665dbb434da5acaec045
cschlick <cjschlicksup@lbl.gov>
2022-04-03 01:09:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='PhenixML',
     version='0.4dev',
-    packages=['phenixml',],
+    packages=['phenixml','fragments',],
     license=open('LICENSE.txt').read(),
     long_description=open('README.md').read(),
 )

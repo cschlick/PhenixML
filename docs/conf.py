3fcde8fc445432615e8e8b3ecce4d57cc9ee029b
cschlick <cjschlicksup@lbl.gov>
2022-04-02 22:47:26 -0700

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,7 @@ author = 'cschlick'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    ...
-    'sphinx_rtd_theme',
+    'sphinx_rtd_theme'
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

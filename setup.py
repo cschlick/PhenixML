07fe3b0c7c5aec33616a7fe391c9af8e14b783aa
cschlick <cjschlicksup@lbl.gov>
2022-04-03 11:57:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.4dev',
     packages=['phenixml','phenixml.fragments',"phenixml.featurizers","phenixml.fragmentation","phenixml.visualization","phenixml.graphs","phenixml.models",
              "phenixml.labelers","phenixml.utils"],
+    install_requires=["rdkit-pypi","torch","scikit-learn","numpy","scipy","matplotlib"],
     license=open('LICENSE.txt').read(),
     long_description=open('README.md').read(),
 )
